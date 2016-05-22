fbe0a6791d735522aebb9e67e27517f839604057
saumya <2saumyaray@gmail.com>
2016-05-22 17:42:16 +0530

    graphics added

--- Source/game/Game.hx
+++ Source/game/Game.hx
@@ -10,16 +10,51 @@ import openfl.system.Capabilities;
 import openfl.Assets;
 import openfl.Lib;
 
+import openfl.display.Graphics;
+import openfl.display.Shape;
+
+import utils.ShapeUtil;
+
 class Game extends Sprite {
 	
+	private var shapeUtil:ShapeUtil;
+	private var background:Shape;
+	private var shapeContainer:Sprite;
+
 	public var currentScale:Float;
+
 
 	public function new(){
 		super();
+		init();
 	}
-	public function init():Void{}
-	public function construct():Void{}
-	public function render():Void{}
+	public function init():Void{
+		this.shapeUtil = new ShapeUtil();
+		construct();
+	}
+	public function construct():Void{
+
+		this.background = this.shapeUtil.getBackground();
+		this.addChild(this.background);
+
+		this.shapeContainer = new Sprite();
+		this.addChild(this.shapeContainer);
+
+		
+
+		this.addEventListener(Event.ENTER_FRAME,render);
+	}
+	
+	public function render(e:Event):Void{
+		
+		this.shapeContainer.removeChildren();
+
+		var s = shapeUtil.getCircle(50);
+		this.shapeContainer.addChild(s);
+
+		
+	}
+
 	public function resize(newWidth:Int, newHeight:Int):Void {
 		var maxWidth = newWidth * 0.90;
 		var maxHeight = newHeight * 0.86;
---
+++ Source/utils/ShapeUtil.hx
@@ -0,0 +1,36 @@
+//
+package utils;
+
+import openfl.display.Shape;
+import openfl.display.Graphics;
+
+import openfl.geom.Point;
+
+class ShapeUtil {
+
+	private var maxColorValue:UInt;
+	private var bgSize:Point;
+
+	public function new() {
+		this.maxColorValue = 256*256*256;
+		this.bgSize = new Point(1017,800);
+	}
+	public function getBackground():Shape{
+		var s:Shape = new Shape();
+		var g:Graphics = s.graphics;
+		var c:UInt = Math.round(Math.random()*this.maxColorValue);
+		g.beginFill(c,1.0);
+		g.drawRect(0,0,bgSize.x,bgSize.y);
+		g.endFill();
+		return s;
+	}
+	public function getCircle(radius:UInt):Shape{
+		var s:Shape = new Shape();
+		var g:Graphics = s.graphics;
+		var c:UInt = Math.round(Math.random()*this.maxColorValue);
+		g.beginFill(c,1.0);
+		g.drawCircle(0,0,radius);
+		g.endFill();
+		return s;
+	}
+}

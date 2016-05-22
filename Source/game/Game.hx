2bded6128f85311364958b4f455985c1b3721292
saumya <2saumyaray@gmail.com>
2016-05-22 12:39:42 +0530

    wip game

---
+++ Source/game/Game.hx
@@ -0,0 +1,23 @@
+//
+package game;
+
+import openfl.display.Bitmap;
+import openfl.display.BitmapData;
+import openfl.display.Sprite;
+import openfl.events.Event;
+import openfl.events.KeyboardEvent;
+import openfl.system.Capabilities;
+import openfl.Assets;
+import openfl.Lib;
+
+class Game extends Sprite {
+	public function Game(){
+		super()
+	}
+	public function init():Void{}
+	public function construct():Void{}
+	public function render():Void{}
+	public function resize(newWidth:Int, newHeight:Int):Void {
+		
+	}
+}

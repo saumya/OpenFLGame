d625d0cd8281107a7ae3646bcc556a6afa0b02ab
saumya <2saumyaray@gmail.com>
2016-05-24 11:19:15 +0530

    function added: to get a random color

--- Source/utils/ShapeUtil.hx
+++ Source/utils/ShapeUtil.hx
@@ -28,10 +28,14 @@ class ShapeUtil {
 	public function getCircle(radius:UInt):Shape{
 		var s:Shape = new Shape();
 		var g:Graphics = s.graphics;
-		var c:UInt = Math.round(Math.random()*this.maxColorValue);
-		g.beginFill(c,1.0);
+		g.beginFill(getARandomColor(),1.0);
 		g.drawCircle(0,0,radius);
 		g.endFill();
 		return s;
+	}
+	public function getARandomColor():UInt{
+		var i:Float = Math.random()*this.maxColorValue;
+		var j:UInt = Math.round(i);
+		return j;
 	}
 }

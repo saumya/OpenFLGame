92818a3147b939661e2b97ebe69343ef95c51421
saumya <2saumyaray@gmail.com>
2016-05-26 14:18:30 +0530

    made the button of flexible size

--- Source/utils/ButtonFactory.hx
+++ Source/utils/ButtonFactory.hx
@@ -9,6 +9,8 @@ import openfl.geom.Point;
 import openfl.text.TextField;
 import openfl.text.TextFormat;
 import openfl.text.TextFormatAlign;
+import openfl.text.TextFieldAutoSize;
+
 import openfl.Assets;
 import openfl.Lib;
 
@@ -21,33 +23,41 @@ class ButtonFactory {
 	}
 
 	public function getQuickButton():Sprite{
-		var s:Sprite = this.getRectangle();
+		//
+		var horizontalMargin:UInt = 10;
+		var fontHeight:UInt = 100;
 		// text formats
 		//var font = Assets.getFont ("fonts/FreebooterUpdated.ttf");
 		var font = Assets.getFont ("fonts/Archistico_Simple.ttf");
-		var defaultFormat = new TextFormat (font.fontName, 100, 0x000000);
+		var defaultFormat = new TextFormat (font.fontName, fontHeight, 0x000000);
 		defaultFormat.align = TextFormatAlign.CENTER;
 		// text
 		var t:TextField = new TextField();
 		t.text = "Saumya";
-		t.width = 300;
-		t.height = 100;
-		t.x = 0;
-		t.y = -10; // A little upward is actually makes it look in CENTER
+		//t.width = 300;
+		//t.height = 100;
+		t.autoSize = TextFieldAutoSize.LEFT;
+		t.x = 0+horizontalMargin;
+		t.y = -(fontHeight/10); // A little upward is actually makes it look in CENTER
 		t.defaultTextFormat = defaultFormat;
 		t.embedFonts = true;
 		t.selectable = false;
+		//
+		trace(t.width,t.height);
+		var s:Sprite = this.getRectangle(t.width,t.height,horizontalMargin);
 		// add the text
 		s.addChild(t);
 		return s;
 	}
 
-	private function getRectangle():Sprite {
+	private function getRectangle(w:Float,h:Float,margin:Float):Sprite {
+		var width:Float = w + (2*margin);
+
 		var s:Sprite = new Sprite();
 		var g:Graphics = s.graphics;
 		var c:UInt = Math.round(Math.random()*this.maxColorValue);
 		g.beginFill(c,1.0);
-		g.drawRect(0,0,300,100);
+		g.drawRect(0,0,width,h);
 		g.endFill();
 		return s;
 	}

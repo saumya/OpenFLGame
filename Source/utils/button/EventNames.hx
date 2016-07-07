3393c081105ccc15142a589e8e641a6b48ae8364
saumya <2saumyaray@gmail.com>
2016-07-08 00:49:11 +0530

    cleanup of un used files

--- Source/utils/button/EventNames.hx
+++
@@ -1,9 +0,0 @@
-package utils.button;
-
-
-class EventNames {
-	public static var GAME_RESTART:String = "restart";
-	public static var GAME_NEW_PATTERN:String = "newPatternStyle";
-	public static var GAME_PAUSE:String = "pause";
-	public static var GAME_END:String = "end";
-}
